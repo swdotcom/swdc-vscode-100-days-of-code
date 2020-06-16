670f2cfded6eb8a0fbdda64ee59887d2ca936aa3
dillon-sftw <dillon@software.com>
2020-06-16 11:04:57 -0500

    Updated Logs UI

--- src/utils/CommandUtil.ts
+++ src/utils/CommandUtil.ts
@@ -1,7 +1,7 @@
 import { Disposable, commands, window, TreeView, ViewColumn } from "vscode";
 import { TreeNode } from "../models/TreeNode";
 import { Tree100DoCProvider, connectDoCTreeView } from "../tree/Tree100DoCProvider";
-import { getLogsHtml, updateLogsHtml, addLogToJson, editLogEntry, updateLogShare } from "./LogsUtil";
+import { getLogsHtml, updateLogsHtml, addLogToJson, editLogEntry, updateLogShare, editLogHours } from "./LogsUtil";
 import {
     updateMilestonesHtml,
     getMilestonesHtml,
@@ -48,6 +48,7 @@ export function createCommands(): { dispose: () => void } {
                                 dayUpdate.description,
                                 dayUpdate.links
                             );
+                            editLogHours(parseInt(dayUpdate.day_number), parseFloat(dayUpdate.hours));
                             break;
                         case "addLog":
                             panel.dispose();
